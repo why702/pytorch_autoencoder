a62ebfed4b40959776d6070ed938f1f36e4d7be2
bill.wang <bill.wang@egistec.com>
2020-03-13 16:08:04 +0800

    add ar_snr_pytorch

--- util.py
+++ util.py
@@ -3,11 +3,21 @@ import numpy as np
 import struct
 import cv2
 import math
-
+import os
 
 def read_bin(bin_path, tuple_size=(200, 200), low_endian=True):
     f = open(bin_path, "r")
     byte = np.fromfile(f, dtype=np.uint16)
+
+    if low_endian == False:
+        for i in range(len(byte)):
+            b = struct.pack('>H', byte[i])
+            byte[i] = struct.unpack('H', b)[0]
+    return byte.reshape(tuple_size)
+
+def read_8bit_bin(bin_path, tuple_size=(200, 200), low_endian=True):
+    f = open(bin_path, "r")
+    byte = np.fromfile(f, dtype=np.uint8)
 
     if low_endian == False:
         for i in range(len(byte)):
@@ -38,6 +48,8 @@ def convert_lbp(byte):
 
 def substract(nd1, nd2):
     diff = np.subtract(nd1.astype(np.float32), nd2.astype(np.float32))
+    min = np.min(diff)
+    diff -= min
     return diff
 
 
@@ -106,6 +118,78 @@ def LPF_FWHM(byte, LPF):
     Image_orig_filtered = np.real(
         np.fft.ifft2((np.multiply(Image_orig_f, expo))))
     return Image_orig_filtered
+
+def read_bins(bin_dir, width, height, low_endian, FORMAT = 0):
+    img_list = []
+    bk_list = []
+    ipp_list = []
+    bds_list = []
+    img = None
+    bk = None
+    ipp = None
+    bds = None
+    BK_et = 0
+    need_bk = True
+
+    for root, dirs, files in os.walk(bin_dir, topdown=False):
+        for name in files:
+            # print(os.path.join(root, name))
+
+            if os.path.splitext(name)[1] == '.bin':
+
+                if FORMAT == 702:
+                    # if root.find("image_raw") != -1:
+                    #     img = util.read_bin(os.path.join(root, name),
+                    #                         (height, width), low_endian)
+                    #     img = np.pad(img, ((2, 2), (1, 1)), 'reflect')
+                    # if img is None:
+                    #     continue
+                    #
+                    # # diff = util.normalize_ndarray(img) * 255
+                    # # cv2.imshow("", diff.astype(np.uint8))
+                    # # cv2.waitKey()
+                    #
+                    # self.input_list.append(img)
+                    pass
+                else:
+
+                    if name.find("_Img16b_") != -1:
+
+                        #find et
+                        et = name[name.find("_et=") + 4: name.find("_hc=")]
+
+                        #find bk
+                        mi = name[name.find("mica=") + 5: name.find("mica=") + 7]
+
+                        if root.find("enroll") != -1 and mi == "00" and need_bk:
+                            bk_name = name.replace("Img16b", "Img16bBkg")
+                            bk = read_bin(os.path.join(root, bk_name))
+                            need_bk = False
+                            BK_et = et
+                        elif need_bk == False and root.find("enroll") == -1:
+                            need_bk = True
+
+                        if BK_et != et or et == 0:
+                            continue
+
+                        img = read_bin(os.path.join(root, name))
+
+                        ipp_name = name.replace("Img16b", "Img8b")
+                        ipp = read_8bit_bin(os.path.join(root, ipp_name))
+
+                        bds_name = name.replace("Img16b", "Img16bBkg")
+                        bds = read_bin(os.path.join(root, bds_name))
+
+                        if img is None or bk is None or ipp is None or bds is None:
+                            continue
+
+                        img_list.append(img)
+                        bk_list.append(bk)
+                        ipp_list.append(ipp)
+                        bds_list.append(bds)
+
+    print("img_list size is {}".format(len(img_list)))
+    return img_list, bk_list, ipp_list, bds_list
 
 
 if __name__ == '__main__':
